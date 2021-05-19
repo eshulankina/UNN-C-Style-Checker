5a1f004165645fdc759932a7858c594c8f3d5145
katsum25 <pestreev00@mail.ru>
2021-05-19 16:30:43 +0300

    Complete c-style-cast-checker task

--- main.cpp
+++ main.cpp
@@ -18,13 +18,35 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
+private:
+    Rewriter& rewriter_;
 public:
-    CastCallBack(Rewriter& rewriter) {
+    CastCallBack(Rewriter& rewriter) : rewriter_(rewriter) {
         // Your code goes here
+	
     };
 
     void run(const MatchFinder::MatchResult &Result) override {
         // Your code goes here
+	const auto *nodeCastExpr = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+	if (nodeCastExpr == nullptr) return;
+
+	StringRef DTypeS = Lexer::getSourceText(CharSourceRange::getTokenRange(
+                nodeCastExpr->getLParenLoc().getLocWithOffset(1),
+                nodeCastExpr->getRParenLoc().getLocWithOffset(-1)),
+                *Result.SourceManager, Result.Context->getLangOpts());
+
+	auto resStart = ("static_cast<" + DTypeS + ">(").str();
+
+	auto replaceRange = CharSourceRange::getCharRange(
+            nodeCastExpr->getLParenLoc(),
+            nodeCastExpr->getSubExprAsWritten()->getBeginLoc());
+	
+	rewriter_.ReplaceText(replaceRange, resStart);
+
+	auto subExpr = nodeCastExpr->getSubExprAsWritten()->IgnoreImpCasts();
+	rewriter_.InsertText(Lexer::getLocForEndOfToken(subExpr->getEndLoc(), 0,
+							*Result.SourceManager, Result.Context->getLangOpts()), ")");
     }
 };
 
@@ -65,7 +87,7 @@ private:
 static llvm::cl::OptionCategory CastMatcherCategory("cast-matcher options");
 
 int main(int argc, const char **argv) {
-    auto Parser = llvm::ExitOnError()(CommonOptionsParser::create(argc, argv, CastMatcherCategory));
+    CommonOptionsParser Parser(argc, argv, CastMatcherCategory);
 
     ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
     return Tool.run(newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
