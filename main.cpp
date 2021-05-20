e4df1e8832801993d6ac0f51be2224594e9326d9
Tkachev Alexey <demousegun@gmail.com>
2021-05-20 19:55:43 +0300

    Initial

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <sstream>
 
 #include <llvm/Support/CommandLine.h>
@@ -18,13 +19,68 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
+private:
+    Rewriter* rewriter__=nullptr;
+
 public:
     CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
+        this->rewriter__ = &rewriter;
     };
 
-    void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
+    void run(const MatchFinder::MatchResult& Result) override {
+        const auto* C_STYLE =
+                Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+
+        if (!C_STYLE) {
+            return;
+        }
+
+        const int OFFSET_FOR_FROM = 1;
+        const int OFFSET_FOR_UP_TO = -1;
+        
+        SourceLocation range_from  =
+                C_STYLE->getLParenLoc().getLocWithOffset(OFFSET_FOR_FROM);
+
+        SourceLocation range_up_to =
+                C_STYLE->getRParenLoc().getLocWithOffset(OFFSET_FOR_UP_TO);
+
+        const auto TOKEN_RANGE =
+                CharSourceRange::getTokenRange(range_from, range_up_to);
+
+        const StringRef CAST_FROM = Lexer::getSourceText(TOKEN_RANGE,
+                                                        *Result.SourceManager,
+                                                        Result.Context->getLangOpts());
+
+       std::string CAST =
+                "static_cast<" + CAST_FROM.str() + ">";
+
+        const auto LOC =
+                C_STYLE->getSubExprAsWritten()->IgnoreImpCasts();
+
+        range_from =
+                C_STYLE->getLParenLoc();
+
+        range_up_to =
+                C_STYLE->getSubExprAsWritten()->getBeginLoc();
+
+        const auto CHAR_RANGE =
+                CharSourceRange::getCharRange(range_from, range_up_to);
+
+        CAST += "(";
+
+        this->rewriter__->ReplaceText(CHAR_RANGE, CAST);
+
+        const LangOptions OPTIONS = Result.Context->getLangOpts();
+        
+        const SourceLocation END_LOC = LOC->getEndLoc();
+
+        const unsigned int OFFSET = 0;
+
+        const SourceLocation END_TOKEN = Lexer::getLocForEndOfToken(END_LOC, 
+                                                        OFFSET, 
+                                                        *Result.SourceManager, 
+                                                        OPTIONS);
+        this->rewriter__->InsertText(END_TOKEN , ")");
     }
 };
 
@@ -65,7 +121,7 @@ private:
 static llvm::cl::OptionCategory CastMatcherCategory("cast-matcher options");
 
 int main(int argc, const char **argv) {
-    auto Parser = llvm::ExitOnError()(CommonOptionsParser::create(argc, argv, CastMatcherCategory));
+    CommonOptionsParser Parser(argc, argv, CastMatcherCategory);
 
     ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
     return Tool.run(newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
