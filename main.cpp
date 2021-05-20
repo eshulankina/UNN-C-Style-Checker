a32571b5fe44b3e06a0677131050f9764061f2e2
root <root@LAPTOP-MVTI7E6V.localdomain>
2021-05-20 16:16:06 +0300

    Complete c-style-cast-checker task

--- main.cpp
+++ main.cpp
@@ -19,13 +19,21 @@ using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
 public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
+    CastCallBack(Rewriter& rewriter): rewriter1(rewriter) {};
 
     void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
-    }
+        const auto *castexp = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+	auto &sourcem = *Result.SourceManager;
+	auto reprange = CharSourceRange::getCharRange (castexp->getLParenLoc(), castexp->getSubExprAsWritten()->getBeginLoc());
+	auto typeofstr = Lexer::getSourceText(CharSourceRange::getTokenRange(castexp->getLParenLoc().getLocWithOffset(1), castexp->getRParenLoc().getLocWithOffset(-1)), sourcem, Result.Context->getLangOpts());
+	auto str = ("static_cast<" + typeofstr + ">").str();
+	rewriter1.ReplaceText(reprange, str);
+        const auto *ignsubexp = castexp->getSubExprAsWritten()->IgnoreImpCasts();
+	auto esubexp = Lexer::getLocForEndOfToken(ignsubexp->getEndLoc(), 0, sourcem, Result.Context->getLangOpts());
+        rewriter1.InsertText(esubexp,")");
+												     }
+private:
+	Rewriter& rewriter1;
 };
 
 class MyASTConsumer : public ASTConsumer {
@@ -65,7 +73,7 @@ private:
 static llvm::cl::OptionCategory CastMatcherCategory("cast-matcher options");
 
 int main(int argc, const char **argv) {
-    auto Parser = llvm::ExitOnError()(CommonOptionsParser::create(argc, argv, CastMatcherCategory));
+    CommonOptionsParser Parser(argc, argv, CastMatcherCategory);
 
     ClangTool Tool(Parser.getCompilations(), Parser.getSourcePathList());
     return Tool.run(newFrontendActionFactory<CStyleCheckerFrontendAction>().get());
