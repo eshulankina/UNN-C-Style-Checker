7790d0b0eb095c1e1d4e26625a309478c0a9366b
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-05-26 13:53:54 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -18,14 +18,32 @@ using namespace clang::ast_matchers;
 using namespace clang::tooling;
 
 class CastCallBack : public MatchFinder::MatchCallback {
-public:
-    CastCallBack(Rewriter& rewriter) {
-        // Your code goes here
-    };
+ public:
+    CastCallBack(Rewriter& rewriter) : rewriter_(rewriter) {};
 
-    void run(const MatchFinder::MatchResult &Result) override {
-        // Your code goes here
+    virtual void run(const MatchFinder::MatchResult& Result) {
+	  const auto *CastExpr = Result.Nodes.getNodeAs<CStyleCastExpr>("cast");
+	  auto &SM = *Result.SourceManager;
+
+	  auto DestTypeString = Lexer::getSourceText(CharSourceRange::getTokenRange(
+                                 CastExpr->getLParenLoc().getLocWithOffset(1),
+                                 CastExpr->getRParenLoc().getLocWithOffset(-1)),
+                                 SM, Result.Context->getLangOpts());
+
+	  auto s = ("static_cast<" + DestTypeString + ">(").str();
+	  auto Range = CharSourceRange::getCharRange(
+			  CastExpr->getLParenLoc(),
+			  CastExpr->getSubExprAsWritten()->getBeginLoc());
+
+	  rewriter_.ReplaceText(Range, s);
+
+	  const auto *SubExpr = CastExpr->getSubExprAsWritten()->IgnoreImpCasts();
+	  auto EndSubExpr = Lexer::getLocForEndOfToken(SubExpr->getEndLoc(), 0, SM, Result.Context->getLangOpts());
+
+ 	  rewriter_.InsertText(EndSubExpr,")");
     }
+private:
+    Rewriter& rewriter_;
 };
 
 class MyASTConsumer : public ASTConsumer {
