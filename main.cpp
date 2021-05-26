65ab15575a7b06a535047befe0e80d0440f6b63e
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-05-26 22:25:52 +0300

    fix3

--- main.cpp
+++ main.cpp
@@ -21,28 +21,22 @@ class CastCallBack : public MatchFinder::MatchCallback {
  public:
     CastCallBack(Rewriter& rewriter) : rewriter_(rewriter) {};
 
-    virtual void run(const MatchFinder::MatchResult &Result) {
+    void run(const MatchFinder::MatchResult &Result) override {
     	if (const auto *Cast_Term = Result.Nodes.getNodeAs<CStyleCastExpr>("cast")) {
 		if (Cast_Term->getCastKind() == CK_ToVoid)
 			return;
 		if (Cast_Term->getExprLoc().isMacroID())
 			return;
-
-
+		
 		auto Replace_Range = CharSourceRange::getCharRange(
 				        Cast_Term->getLParenLoc(), Cast_Term->getSubExprAsWritten()->getBeginLoc());
-
 		StringRef String_Type = Lexer::getSourceText(CharSourceRange::getTokenRange(
 					              	Cast_Term->getLParenLoc().getLocWithOffset(1),
 						        Cast_Term->getRParenLoc().getLocWithOffset(-1)),
 				              		*Result.SourceManager, Result.Context->getLangOpts());
-
 		std::string Text_Type(("static_cast<" + String_Type + ">").str());
-
 		const auto *Cast_Ignore = Cast_Term->getSubExprAsWritten()->IgnoreImpCasts();
-
-		if (!isa<ParenExpr>(Cast_Ignore)) {
-			
+		if (!isa<ParenExpr>(Cast_Ignore)) {	
 			Text_Type.push_back('(');
 			rewriter_.InsertText(Lexer::getLocForEndOfToken(Cast_Ignore->getEndLoc(),
 						   0, *Result.SourceManager, Result.Context->getLangOpts()), ")");
