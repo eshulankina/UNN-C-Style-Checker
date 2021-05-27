c3e5ba1f572a94fe5e18ad056911f2c734b39dcd
Stanislav Yurin <stanislav.unn@gmail.com>
2021-05-27 10:17:07 +0300

    adding Parens and correct work witn composite types

--- main.cpp
+++ main.cpp
@@ -27,12 +27,15 @@ public:
         if (const auto *F =
                 Result.Nodes.getNodeAs<clang::CStyleCastExpr>("cast")) {
             const auto& SM = *Result.SourceManager;
-            const auto& BeginLoc = F->getBeginLoc();
-            const auto& EndLoc = F->getEndLoc();
+            const auto& BeginLoc = F->getLParenLoc();
+            const auto& EndLoc = F->getRParenLoc();
             rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
                 .ReplaceText(SM.getFileOffset(BeginLoc), 1, "static_cast<");
             rewriter_.getEditBuffer(rewriter_.getSourceMgr().getMainFileID())
-                .ReplaceText(SM.getFileOffset(EndLoc) - 1, 1, ">");
+                .ReplaceText(SM.getFileOffset(EndLoc), 1, ">");
+
+            rewriter_.InsertTextBefore(F->getSubExpr()->getBeginLoc(), "(");
+            rewriter_.InsertTextAfterToken(F->getSubExpr()->getEndLoc(), ")");
         }
     }
 
